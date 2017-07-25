9743aa015163254e59b26c623c853ba5f1d0e3b6
Iskander Gaba <iskandergaba@gmail.com>
2017-07-25 23:55:21 +0100

    Init app repository

---
+++ app/src/main/java/com/iskandergaba/something/MainActivity.java
@@ -0,0 +1,12 @@
+package com.iskandergaba.something;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
